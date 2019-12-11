1313ccee939d2046b97cda1d7a49d5691fcc1bcf
Wiktoria Uramowska <35852414+wiktoriauramowska@users.noreply.github.com>
2019-12-11 13:58:00 +0000

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!" 
-
+    return render_template("index.html")
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run()
